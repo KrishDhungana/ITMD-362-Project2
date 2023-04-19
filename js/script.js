a0aba5e67c500d289670cae67c7c668934064a12
KrishDhungana <kdhungana@hawk.iit.edu>
2023-04-19 18:54:52 -0500

    Merging JS files together
    Adding in js functions into one document

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ function sayCode() {
 
 // Call the function
 document.getElementById("myBtn").onclick = sayCode;
+
+function validate() {
+  var name = document.forms["myForm"]["name"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var message = document.forms["myForm"]["message"].value;
+  
+  if (name == "" || email == "" || message == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+}
